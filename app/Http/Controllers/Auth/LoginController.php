4d447f1a058f45fb2ec6ea6c827d8c80003dff38
muhammadikbal512 <muhammadikbal511@gmail.com>
2021-04-01 15:02:45 +0700

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    //redirect user to the google authentication page
+    public function redirectToProvider() 
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    //obtain the user information from GitHub
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->user();
+    }
+
+    public function retrieveUser($user)
+    {
+        $user = Socialite::driver('google')->userFromToken($user);
+
+        return response()->json($user, 200);
+    }
+}
