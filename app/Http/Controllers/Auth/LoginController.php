a33869bf8fd638629d2f486884c11aee18afb727
muhammadikbal512 <muhammadikbal511@gmail.com>
2021-04-03 10:44:04 +0700

    add jwt and socialite

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,25 +4,26 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Laravel\Socialite\Facades\Socialite;
+// use Laravel\Socialite\Facades\Socialite;
+use Socialite;
 
 class LoginController extends Controller
 {
     //redirect user to the google authentication page
     public function redirectToProvider() 
     {
-        return Socialite::driver('google')->redirect();
+        return \Socialite::driver('google')->redirect();
     }
 
     //obtain the user information from GitHub
     public function handleProviderCallback()
     {
-        $user = Socialite::driver('google')->user();
+        $user = \Socialite::driver('google')->user();
     }
 
     public function retrieveUser($user)
     {
-        $user = Socialite::driver('google')->userFromToken($user);
+        $user = \Socialite::driver('google')->userFromToken($user);
 
         return response()->json($user, 200);
     }
